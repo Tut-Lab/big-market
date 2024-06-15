a24247d0d66d868880d6fc51e35c1a2be4db4aaf
zsj <352326430@qq.com>
2024-06-15 19:34:17 +0800

    feat: [营销服务] 第2节 基础层持久化数据

---
+++ big-market-infrastructure/src/main/java/com/tut/infrastructure/persistent/dao/IAwardDao.java
@@ -0,0 +1,12 @@
+package com.tut.infrastructure.persistent.dao;
+
+import com.tut.infrastructure.persistent.po.Award;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IAwardDao {
+
+   List<Award> queryAwardList();
+}
---
+++ big-market-infrastructure/src/main/java/com/tut/infrastructure/persistent/dao/IStrategyDao.java
@@ -0,0 +1,7 @@
+package com.tut.infrastructure.persistent.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IStrategyDao  {
+}
